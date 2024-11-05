0d89370887ab38a38b174d4e396495daebe126ab
Sunny Lin <sunnylin@mirle.com.tw>
2024-11-05 10:58:41 +0800

    ret: separate menus setup as a function setup_menus, and plugin to follow

--- funlab/sched/service.py
+++ funlab/sched/service.py
@@ -37,6 +37,8 @@ class SchedService(ServicePlugin):
             # self._scheduler.add_listener(self._listener_job_start, EVENT_JOB_EXECUTED | EVENT_JOB_ERROR)  # Add this line
             # self._scheduler.add_listener(self._listener_job_removed, EVENT_JOB_REMOVED)  # Add this line
 
+    def setup_menus(self):
+        super().setup_menus()
         mi = MenuItem(title='Sched Tasks',
                 icon='<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-calendar-stats" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">\
                         <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>\
